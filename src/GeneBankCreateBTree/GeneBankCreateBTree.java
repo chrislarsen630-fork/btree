6cf85c08584ae2a90ebd9854334a26659bacfb7e
Christopher M. Larsen <chrislarsen630@u.boisestate.edu>
2018-11-05 20:59:21 -0700

    Basic folder structure.

---
+++ src/GeneBankCreateBTree/GeneBankCreateBTree.java
@@ -0,0 +1,13 @@
+public class GeneBankCreateBTree{
+
+
+
+public static void main(String[] args){
+  System.out.println("GeneBankCreateBTree was properly installed");
+  (new Common.NullClass()).info();
+}
+
+
+
+} // class GeneBankCreateBTree
+
---
+++ src/GeneBankSearch/GeneBankSearch.java
@@ -0,0 +1,13 @@
+public class GeneBankSearch{
+
+
+
+public static void main(String[] args){
+  System.out.println("GeneBankSearch was properly installed");
+  (new Common.NullClass()).info();
+}
+
+
+
+} // class GeneBankSearch
+
