1c86f4bb3e9381c7e39f1b2893095911c32b9be6
Christopher M. Larsen <chrislarsen630@u.boisestate.edu>
2018-11-14 05:35:38 -0700

    Added OmniException.

--- src/GeneBankCreateBTree/DebugFileDumpInterface.java
+++ src/GeneBankCreateBTree/DebugFileDumpInterface.java
@@ -6,8 +6,9 @@ public interface DebugFileDumpInterface{
 
 /** Dumps the BTree to file.
  * @param tree BTree to dump.
- * @param file File path of the target file.                                  */
-public void dumpBTreeToFile(Common.BTreeInterface tree,String file);
+ * @param file File path of the target file.
+ * @throws Common.OmniException on file access or read error. */
+public void dumpBTreeToFile(Common.BTreeInterface tree,String file) throws Common.OmniException;
 
 
 
--- src/GeneBankCreateBTree/GeneBankFileInterface.java
+++ src/GeneBankCreateBTree/GeneBankFileInterface.java
@@ -7,8 +7,9 @@ public interface GeneBankFileInterface{
 /** Associates the GeneBankFileInterface object with a file and sets the file
   * parser to the beginning of the DNA sequence.
   * @param file GeneBank file to load.                                        
-  * @param windowSize Number of DNA bases to load during readData().          */
-public void loadFromFile(String file,int windowSize);
+  * @param windowSize Number of DNA bases to load during readData().
+  * @throws Common.OmniException on file access or read error.                */
+public void loadFromFile(String file,int windowSize) throws Common.OmniException;
 
 
 /** Indicates whether the file parser is at the end of the current DNA
@@ -29,12 +30,14 @@ public boolean hasMoreSubsequences();
   * windowSize set in loadFromFile(). The bases are packed so that the base
   * that was read last is contained in the two least significant bits; for
   * instance, the sequence ACGT is represented as 0b00011011.
-  * @return Packed binary representation of data.                             */
-public long readData();
+  * @return Packed binary representation of data.
+  * @throws Common.OmniException on file access or read error.                */
+public long readData() throws Common.OmniException;
 
 
-/** Sets the file parser to the beginning of the next DNA subsequence.        */
-public void nextSubsequence();
+/** Sets the file parser to the beginning of the next DNA subsequence.
+  * @throws Common.OmniException if there are no more subsequences.           */
+public void nextSubsequence() throws Common.OmniException;
 
 
 
